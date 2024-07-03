f998698862407db8538598f7112b4866001adb13
jyt6640 <jyt6640@gmail.com>
2024-07-03 11:26:10 +0900

    navbar 링크설정

--- crud-app/src/components/Navbar.js
+++ crud-app/src/components/Navbar.js
@@ -23,10 +23,10 @@ const Navbar = () => {
     <div className={`navbar ${navBackground ? "navbar-scrolled" : ""}`}>
       <img className="logo" src="/img/yANUs.png" alt="yANUs Logo" />
       <ul className="nav-links">
-        <li>홈</li>
-        <li>소개</li>
-        <li>서비스 사용법</li>
-        <li>FAQ</li>
+        <li><a href="/home">홈</a></li>
+        <li><a href="/about">소개</a></li>
+        <li><a href="/services">서비스 사용법</a></li>
+        <li><a href="/faq">FAQ</a></li>
       </ul>
       <div className="login">
         <a href="/login">LOGIN</a>
